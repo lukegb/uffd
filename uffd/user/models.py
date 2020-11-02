lukegb/uffd

commit a827f27bebc5a795ae4fd1abc382427666a7b527

added ldap uuid to user object and oauth userinfo endpoint

uffd/user/models.py
@@ -7,13 +7,14 @@ from flask import current_app
 from uffd import ldap
 
 class User():
-	def __init__(self, uid=None, loginname='', displayname='', mail='', groups=None, dn=None):
+	def __init__(self, uid=None, loginname='', displayname='', mail='', groups=None, dn=None, uuid=None):
 		self.uid = uid
 		self.loginname = loginname
 		self.displayname = displayname
 		self.mail = mail
 		self.newpassword = None
 		self.dn = dn
+		self.uuid = uuid
 
 		self.groups_ldap = groups or []
 		self.initial_groups_ldap = groups or []
@@ -29,6 +30,7 @@ class User():
 				mail=ldapobject['mail'].value,
 				groups=ldap.get_ldap_array_attribute_safe(ldapobject, 'memberOf'),
 				dn=ldapobject.entry_dn,
+				uuid=ldapobject['entryUUID'].value
 			)
 
 	@classmethod

uffd/oauth2/views.py
@@ -103,6 +103,7 @@ def userinfo():
 		nickname=user.loginname,
 		email=user.mail,
 		ldap_dn=user.dn,
+		ldap_uuid=user.uuid,
 		groups=[group.name for group in user.get_groups()]
 	)
 

commit 4a705c1157926a8066a76a338083d1840547e731

fixed tests after they broke by a827f27bebc5a795ae4fd1abc382427666a7b527

uffd/user/models.py
@@ -7,7 +7,7 @@ from flask import current_app
 from uffd import ldap
 
 class User():
-	def __init__(self, uid=None, loginname='', displayname='', mail='', groups=None, dn=None, uuid=None):
+	def __init__(self, uid=None, loginname='', displayname='', mail='', groups=None, dn=None, uuid=None): # pylint: disable=too-many-arguments
 		self.uid = uid
 		self.loginname = loginname
 		self.displayname = displayname
@@ -30,7 +30,12 @@ class User():
 				mail=ldapobject['mail'].value,
 				groups=ldap.get_ldap_array_attribute_safe(ldapobject, 'memberOf'),
 				dn=ldapobject.entry_dn,
-				uuid=ldapobject['entryUUID'].value
+				# The LDAP mock does not generate UUIDs for newly created LDAP objects,
+				# so we use a dummy value if the attribute is missing (only for testing!)
+				uuid=ldapobject['entryUUID'].value \
+					if 'entryUUID' in ldapobject.entry_attributes_as_dict \
+					or not current_app.config.get('LDAP_SERVICE_MOCK', False) \
+					else '00000000-0000-0000-0000-000000000000'
 			)
 
 	@classmethod