5b93c4f445e659551e3b3ad706ecd57cf28f87e0
Julian Rother <julianr@fsmpi.rwth-aachen.de>
2021-04-13 20:10:39 +0200

    Enable automatic constraint naming and recreate all db tables with named constraints

--- uffd/database.py
+++ uffd/database.py
@@ -1,10 +1,20 @@
 from collections import OrderedDict
 
+from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask.json import JSONEncoder
 
+convention = {
+	'ix': 'ix_%(column_0_label)s',
+	'uq': 'uq_%(table_name)s_%(column_0_name)s',
+	'ck': 'ck_%(table_name)s_%(column_0_name)s',
+	'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+	'pk': 'pk_%(table_name)s'
+}
+metadata = MetaData(naming_convention=convention)
+
 # pylint: disable=C0103
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=metadata)
 # pylint: enable=C0103
 
 class SQLAlchemyJSON(JSONEncoder):
