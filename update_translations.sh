dc938565c38e151ac017a08e083e7d0b6a6e0835
Julian Rother <julianr@fsmpi.rwth-aachen.de>
2021-07-30 19:50:58 +0200

    Added update_translations.sh and CI job that outputs coverage

---
+++ update_translations.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+pybabel extract -F uffd/babel.cfg -k lazy_gettext -o messages.pot uffd
+pybabel update -i messages.pot -d uffd/translations
+pybabel compile -d uffd/translations
+
+if [ -n "$1" ]; then
+	NUM_EMPTY="$(tr '\n' '|' < uffd/translations/$1/LC_MESSAGES/messages.po | sed 's/msgstr ""|/empty/g' | tr '|' '\n' | grep '^empty$' | wc -l)"
+	NUM_TOTAL="$(grep '^msgid' uffd/translations/$1/LC_MESSAGES/messages.po | wc -l)"
+	# Emulate python-coverage output
+	echo "TOTAL $NUM_TOTAL $(( $NUM_TOTAL - $NUM_EMPTY )) $(( 100 * ($NUM_TOTAL - $NUM_EMPTY) / $NUM_TOTAL ))%"
+fi
