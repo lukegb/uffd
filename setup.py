3abcbf208f7b300fff72642427a68d1114915132
Julian <julian@cccv.de>
2021-08-13 15:00:47 +0000

    v1.0.0 release

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
 	name='uffd',
 	version=os.environ.get('PACKAGE_VERSION', 'local'),
-	description='UserFerwaltungsFrontend: Ldap based single sign on and user management web software',
+	description='Ldap based single sign on and user management web software',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	url='https://git.cccv.de/uffd/uffd',
 	classifiers=[
 		'Programming Language :: Python :: 3',
-		'Development Status :: 4 - Beta',
+		'Development Status :: 5 - Production/Stable',
 		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
 		'Operating System :: OS Independent',
 		'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP',
