lukegb/uffd

commit fa67bde07b4eb35708cb49a92e57901f9c7f133d

Migrate OAuth2 and API clients to database
Also adds a shallow Service model that coexists with the config-defined
services to group multiple OAuth2 and API clients together.

Clients defined in the config with OAUTH2_CLIENTS and API_CLIENTS_2 are
imported by the database migrations.

Removes support for complex values for the OAuth2 client group_required option.
Only simple group names are supported, not (nested) lists of groups previously
interpreted as AND/OR conjunctions. Also removes support for the login_message
parameter of OAuth2 clients.

uffd/service/templates/service/overview.html
@@ -59,6 +59,12 @@
   </div>
 {% endmacro %}
 
+{% if request.user and request.user.is_in_group(config['ACL_ADMIN_GROUP']) %}
+<div class="text-right">
+	<a href="{{ url_for('service.index') }}" class="btn btn-primary">{{ _('Manage OAuth2 and API clients') }}</a>
+</div>
+{% endif %}
+
 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 mt-2">
 	{% for service in services if service.has_access %}
 		{{ service_card(service) }}

uffd/service/views.py
@@ -1,0 +1,161 @@
+from flask import Blueprint, render_template, request, url_for, redirect, current_app, abort
+from flask_babel import lazy_gettext
+
+from uffd.navbar import register_navbar
+from uffd.csrf import csrf_protect
+from uffd.session import login_required
+from uffd.service.models import Service, get_services
+from uffd.user.models import Group
+from uffd.oauth2.models import OAuth2Client, OAuth2LogoutURI
+from uffd.api.models import APIClient
+from uffd.database import db
+
+bp = Blueprint('service', __name__, template_folder='templates')
+
+def service_admin_acl():
+	return request.user and request.user.is_in_group(current_app.config['ACL_ADMIN_GROUP'])
+
+def service_overview_acl():
+	return len(get_services(request.user)) > 0 or service_admin_acl()
+
+@bp.route('/services/')
+@register_navbar(lazy_gettext('Services'), icon='sitemap', blueprint=bp, visible=service_overview_acl)
+def overview():
+	services = get_services(request.user)
+	if not service_overview_acl():
+		abort(404)
+	banner = current_app.config.get('SERVICES_BANNER')
+	# Set the banner to None if it is not public and no user is logged in
+	if not (current_app.config['SERVICES_BANNER_PUBLIC'] or request.user):
+		banner = None
+	return render_template('service/overview.html', user=request.user, services=services, banner=banner)
+
+@bp.route('/service/admin')
+@login_required(service_admin_acl)
+def index():
+	return render_template('service/index.html', services=Service.query.all())
+
+@bp.route('/service/new')
+@bp.route('/service/<int:id>')
+@login_required(service_admin_acl)
+def show(id=None):
+	service = Service() if id is None else Service.query.get_or_404(id)
+	all_groups = Group.query.all()
+	return render_template('service/show.html', service=service, all_groups=all_groups)
+
+@bp.route('/service/new', methods=['POST'])
+@bp.route('/service/<int:id>', methods=['POST'])
+@csrf_protect(blueprint=bp)
+@login_required(service_admin_acl)
+def edit_submit(id=None):
+	if id is None:
+		service = Service()
+		db.session.add(service)
+	else:
+		service = Service.query.get_or_404(id)
+	service.name = request.form['name']
+	if not request.form['access-group']:
+		service.limit_access = True
+		service.access_group = None
+	elif request.form['access-group'] == 'all':
+		service.limit_access = False
+		service.access_group = None
+	else:
+		service.limit_access = True
+		service.access_group = Group.query.get(request.form['access-group'])
+	db.session.commit()
+	return redirect(url_for('service.show', id=service.id))
+
+@bp.route('/service/<int:id>/delete')
+@csrf_protect(blueprint=bp)
+@login_required(service_admin_acl)
+def delete(id):
+	service = Service.query.get_or_404(id)
+	db.session.delete(service)
+	db.session.commit()
+	return redirect(url_for('service.index'))
+
+@bp.route('/service/<int:service_id>/oauth2/new')
+@bp.route('/service/<int:service_id>/oauth2/<int:db_id>')
+@login_required(service_admin_acl)
+def oauth2_show(service_id, db_id=None):
+	service = Service.query.get_or_404(service_id)
+	client = OAuth2Client() if db_id is None else OAuth2Client.query.filter_by(service_id=service_id, db_id=db_id).first_or_404()
+	return render_template('service/oauth2.html', service=service, client=client)
+
+@bp.route('/service/<int:service_id>/oauth2/new', methods=['POST'])
+@bp.route('/service/<int:service_id>/oauth2/<int:db_id>', methods=['POST'])
+@csrf_protect(blueprint=bp)
+@login_required(service_admin_acl)
+def oauth2_submit(service_id, db_id=None):
+	service = Service.query.get_or_404(service_id)
+	if db_id is None:
+		client = OAuth2Client(service=service)
+		db.session.add(client)
+	else:
+		client = OAuth2Client.query.filter_by(service_id=service_id, db_id=db_id).first_or_404()
+	client.client_id = request.form['client_id']
+	if request.form['client_secret']:
+		client.client_secret = request.form['client_secret']
+	if not client.client_secret:
+		abort(400)
+	client.redirect_uris = request.form['redirect_uris'].strip().split('\n')
+	client.logout_uris = []
+	for line in request.form['logout_uris'].split('\n'):
+		line = line.strip()
+		if not line:
+			continue
+		method, uri = line.split(' ', 2)
+		client.logout_uris.append(OAuth2LogoutURI(method=method, uri=uri))
+	db.session.commit()
+	return redirect(url_for('service.show', id=service.id))
+
+@bp.route('/service/<int:service_id>/oauth2/<int:db_id>/delete')
+@csrf_protect(blueprint=bp)
+@login_required(service_admin_acl)
+def oauth2_delete(service_id, db_id=None):
+	service = Service.query.get_or_404(service_id)
+	client = OAuth2Client.query.filter_by(service_id=service_id, db_id=db_id).first_or_404()
+	db.session.delete(client)
+	db.session.commit()
+	return redirect(url_for('service.show', id=service.id))
+
+@bp.route('/service/<int:service_id>/api/new')
+@bp.route('/service/<int:service_id>/api/<int:id>')
+@login_required(service_admin_acl)
+def api_show(service_id, id=None):
+	service = Service.query.get_or_404(service_id)
+	client = APIClient() if id is None else APIClient.query.filter_by(service_id=service_id, id=id).first_or_404()
+	return render_template('service/api.html', service=service, client=client)
+
+@bp.route('/service/<int:service_id>/api/new', methods=['POST'])
+@bp.route('/service/<int:service_id>/api/<int:id>', methods=['POST'])
+@csrf_protect(blueprint=bp)
+@login_required(service_admin_acl)
+def api_submit(service_id, id=None):
+	service = Service.query.get_or_404(service_id)
+	if id is None:
+		client = APIClient(service=service)
+		db.session.add(client)
+	else:
+		client = APIClient.query.filter_by(service_id=service_id, id=id).first_or_404()
+	client.auth_username = request.form['auth_username']
+	if request.form['auth_password']:
+		client.auth_password = request.form['auth_password']
+	if not client.auth_password:
+		abort(400)
+	client.perm_users = request.form.get('perm_users') == '1'
+	client.perm_checkpassword = request.form.get('perm_checkpassword') == '1'
+	client.perm_mail_aliases = request.form.get('perm_mail_aliases') == '1'
+	db.session.commit()
+	return redirect(url_for('service.show', id=service.id))
+
+@bp.route('/service/<int:service_id>/api/<int:id>/delete')
+@csrf_protect(blueprint=bp)
+@login_required(service_admin_acl)
+def api_delete(service_id, id=None):
+	service = Service.query.get_or_404(service_id)
+	client = APIClient.query.filter_by(service_id=service_id, id=id).first_or_404()
+	db.session.delete(client)
+	db.session.commit()
+	return redirect(url_for('service.show', id=service.id))

tests/test_services.py
@@ -40,14 +40,14 @@ class TestServices(UffdTestCase):
 		]
 		self.app.config['SERVICES_PUBLIC'] = True
 
-	def test_index(self):
-		r = self.client.get(path=url_for('services.index'))
-		dump('services_index_public', r)
+	def test_overview(self):
+		r = self.client.get(path=url_for('service.overview'))
+		dump('service_overview_public', r)
 		self.assertEqual(r.status_code, 200)
 		self.assertNotIn(b'https://example.com/', r.data)
 		self.login_as('user')
-		r = self.client.get(path=url_for('services.index'))
-		dump('services_index', r)
+		r = self.client.get(path=url_for('service.overview'))
+		dump('service_overview', r)
 		self.assertEqual(r.status_code, 200)
 		self.assertIn(b'https://example.com/', r.data)
 

tests/utils.py
@@ -87,9 +87,13 @@ class UffdTestCase(unittest.TestCase):
 		db.session.add(testadmin)
 		testmail = Mail(uid='test', receivers=['test1@example.com', 'test2@example.com'], destinations=['testuser@mail.example.com'])
 		db.session.add(testmail)
+		self.setUpDB()
 		db.session.commit()
 
 	def setUpApp(self):
+		pass
+
+	def setUpDB(self):
 		pass
 
 	def tearDown(self):

uffd/api/views.py
@@ -1,30 +1,34 @@
 import functools
-import secrets
 
-from flask import Blueprint, jsonify, current_app, request, abort
+from flask import Blueprint, jsonify, request, abort
 
 from uffd.user.models import User, Group
 from uffd.mail.models import Mail, MailReceiveAddress, MailDestinationAddress
+from uffd.api.models import APIClient
 from uffd.session.views import login_get_user, login_ratelimit
 from uffd.database import db
 
 bp = Blueprint('api', __name__, template_folder='templates', url_prefix='/api/v1/')
 
-def apikey_required(scope=None):
+def apikey_required(permission=None):
 	# pylint: disable=too-many-return-statements
+	if permission is not None:
+		assert APIClient.permission_exists(permission)
 	def wrapper(func):
 		@functools.wraps(func)
 		def decorator(*args, **kwargs):
-			if request.authorization and request.authorization.password:
-				if request.authorization.username not in current_app.config['API_CLIENTS_2']:
-					return 'Unauthorized', 401, {'WWW-Authenticate': ['Basic realm="api"']}
-				client = current_app.config['API_CLIENTS_2'][request.authorization.username]
-				if not secrets.compare_digest(request.authorization.password, client['client_secret']):
-					return 'Unauthorized', 401, {'WWW-Authenticate': ['Basic realm="api"']}
-				if scope is not None and scope not in client.get('scopes', []):
-					return 'Forbidden', 403
-			else:
+			if not request.authorization or not request.authorization.password:
 				return 'Unauthorized', 401, {'WWW-Authenticate': ['Basic realm="api"']}
+			client = APIClient.query.filter_by(auth_username=request.authorization.username).first()
+			if not client:
+				return 'Unauthorized', 401, {'WWW-Authenticate': ['Basic realm="api"']}
+			if not client.auth_password.verify(request.authorization.password):
+				return 'Unauthorized', 401, {'WWW-Authenticate': ['Basic realm="api"']}
+			if client.auth_password.needs_rehash:
+				client.auth_password = request.authorization.password
+				db.session.commit()
+			if permission is not None and not client.has_permission(permission):
+				return 'Forbidden', 403
 			return func(*args, **kwargs)
 		return decorator
 	return wrapper
@@ -34,7 +38,7 @@ def generate_group_dict(group):
 	        'members': [user.loginname for user in group.members]}
 
 @bp.route('/getgroups', methods=['GET', 'POST'])
-@apikey_required('getusers')
+@apikey_required('users')
 def getgroups():
 	if len(request.values) > 1:
 		abort(400)
@@ -60,7 +64,7 @@ def generate_user_dict(user, all_groups=None):
 	        'groups': [group.name for group in all_groups if user in group.members]}
 
 @bp.route('/getusers', methods=['GET', 'POST'])
-@apikey_required('getusers')
+@apikey_required('users')
 def getusers():
 	if len(request.values) > 1:
 		abort(400)
@@ -108,7 +112,7 @@ def generate_mail_dict(mail):
 	        'destination_addresses': list(mail.destinations)}
 
 @bp.route('/getmails', methods=['GET', 'POST'])
-@apikey_required('getmails')
+@apikey_required('mail_aliases')
 def getmails():
 	if len(request.values) > 1:
 		abort(400)

uffd/oauth2/views.py
@@ -22,11 +22,8 @@ class UffdRequestValidator(oauthlib.oauth2.RequestValidator):
 	# before anything else. authenticate_client_id would be called instead of authenticate_client for non-confidential
 	# clients. However, we don't support those.
 	def validate_client_id(self, client_id, oauthreq, *args, **kwargs):
-		try:
-			oauthreq.client = OAuth2Client.from_id(client_id)
-			return True
-		except KeyError:
-			return False
+		oauthreq.client = OAuth2Client.query.filter_by(client_id=client_id).one_or_none()
+		return oauthreq.client is not None
 
 	def authenticate_client(self, oauthreq, *args, **kwargs):
 		authorization = oauthreq.extra_credentials.get('authorization')
@@ -41,11 +38,15 @@ class UffdRequestValidator(oauthlib.oauth2.RequestValidator):
 			oauthreq.client_secret = urllib.parse.unquote(authorization.password)
 		if oauthreq.client_secret is None:
 			return False
-		try:
-			oauthreq.client = OAuth2Client.from_id(oauthreq.client_id)
-		except KeyError:
+		oauthreq.client = OAuth2Client.query.filter_by(client_id=oauthreq.client_id).one_or_none()
+		if oauthreq.client is None:
 			return False
-		return secrets.compare_digest(oauthreq.client.client_secret, oauthreq.client_secret)
+		if not oauthreq.client.client_secret.verify(oauthreq.client_secret):
+			return False
+		if oauthreq.client.client_secret.needs_rehash:
+			oauthreq.client.client_secret = oauthreq.client_secret
+			db.session.commit()
+		return True
 
 	def get_default_redirect_uri(self, client_id, oauthreq, *args, **kwargs):
 		return oauthreq.client.default_redirect_uri
@@ -65,7 +66,7 @@ class UffdRequestValidator(oauthlib.oauth2.RequestValidator):
 		return set(scopes).issubset({'profile'})
 
 	def save_authorization_code(self, client_id, code, oauthreq, *args, **kwargs):
-		grant = OAuth2Grant(user=oauthreq.user, client_id=client_id, code=code['code'],
+		grant = OAuth2Grant(user=oauthreq.user, client=oauthreq.client, code=code['code'],
 		                    redirect_uri=oauthreq.redirect_uri, scopes=oauthreq.scopes)
 		db.session.add(grant)
 		db.session.commit()
@@ -80,7 +81,7 @@ class UffdRequestValidator(oauthlib.oauth2.RequestValidator):
 			return False
 		grant_id, grant_code = code.split('-', 2)
 		oauthreq.grant = OAuth2Grant.query.get(grant_id)
-		if not oauthreq.grant or oauthreq.grant.client_id != client_id:
+		if not oauthreq.grant or oauthreq.grant.client != client:
 			return False
 		if not secrets.compare_digest(oauthreq.grant.code, grant_code):
 			return False
@@ -91,13 +92,13 @@ class UffdRequestValidator(oauthlib.oauth2.RequestValidator):
 		return True
 
 	def invalidate_authorization_code(self, client_id, code, oauthreq, *args, **kwargs):
-		OAuth2Grant.query.filter_by(client_id=client_id, code=code).delete()
+		OAuth2Grant.query.filter_by(client=oauthreq.client, code=code).delete()
 		db.session.commit()
 
 	def save_bearer_token(self, token_data, oauthreq, *args, **kwargs):
 		tok = OAuth2Token(
 			user=oauthreq.user,
-			client_id=oauthreq.client.client_id,
+			client=oauthreq.client,
 			token_type=token_data['token_type'],
 			access_token=token_data['access_token'],
 			refresh_token=token_data['refresh_token'],
@@ -138,7 +139,7 @@ class UffdRequestValidator(oauthlib.oauth2.RequestValidator):
 		oauthreq.user = tok.user
 		oauthreq.scopes = scopes
 		oauthreq.client = tok.client
-		oauthreq.client_id = tok.client_id
+		oauthreq.client_id = oauthreq.client.client_id
 		return True
 
 	# get_original_scopes/validate_refresh_token are only used for refreshing tokens. We don't implement the refresh endpoint.
@@ -157,7 +158,7 @@ def handle_oauth2error(error):
 @bp.route('/authorize', methods=['GET', 'POST'])
 def authorize():
 	scopes, credentials = server.validate_authorization_request(request.url, request.method, request.form, request.headers)
-	client = OAuth2Client.from_id(credentials['client_id'])
+	client = OAuth2Client.query.filter_by(client_id=credentials['client_id']).one()
 
 	if request.user:
 		credentials['user'] = request.user
@@ -168,7 +169,7 @@ def authorize():
 			flash(_('We received too many requests from your ip address/network! Please wait at least %(delay)s.', delay=format_delay(host_delay)))
 			return redirect(url_for('session.login', ref=request.full_path, devicelogin=True))
 		host_ratelimit.log()
-		initiation = OAuth2DeviceLoginInitiation(oauth2_client_id=client.client_id)
+		initiation = OAuth2DeviceLoginInitiation(client=client)
 		db.session.add(initiation)
 		try:
 			db.session.commit()
@@ -180,7 +181,7 @@ def authorize():
 		return redirect(url_for('session.devicelogin', ref=request.full_path))
 	elif 'devicelogin_id' in session and 'devicelogin_secret' in session and 'devicelogin_confirmation' in session:
 		initiation = OAuth2DeviceLoginInitiation.query.filter_by(id=session['devicelogin_id'], secret=session['devicelogin_secret'],
-		                                                         oauth2_client_id=client.client_id).one_or_none()
+		                                                         client=client).one_or_none()
 		confirmation = DeviceLoginConfirmation.query.get(session['devicelogin_confirmation'])
 		del session['devicelogin_id']
 		del session['devicelogin_secret']
@@ -192,14 +193,14 @@ def authorize():
 		db.session.delete(initiation)
 		db.session.commit()
 	else:
-		flash(client.login_message)
+		flash(_('You need to login to access this service'))
 		return redirect(url_for('session.login', ref=request.full_path, devicelogin=True))
 
 	# Here we would normally ask the user, if he wants to give the requesting
 	# service access to his data. Since we only have trusted services (the
 	# clients defined in the server config), we don't ask for consent.
 	if not client.access_allowed(credentials['user']):
-		abort(403, description=_("You don't have the permission to access the service <b>%(service_name)s</b>.", service_name=client.client_id))
+		abort(403, description=_("You don't have the permission to access the service <b>%(service_name)s</b>.", service_name=client.service.name))
 	session['oauth2-clients'] = session.get('oauth2-clients', [])
 	if client.client_id not in session['oauth2-clients']:
 		session['oauth2-clients'].append(client.client_id)
@@ -248,5 +249,5 @@ def logout():
 	if not request.values.get('client_ids'):
 		return secure_local_redirect(request.values.get('ref', '/'))
 	client_ids = request.values['client_ids'].split(',')
-	clients = [OAuth2Client.from_id(client_id) for client_id in client_ids]
+	clients = [OAuth2Client.query.filter_by(name=client_id).one() for client_id in client_ids]
 	return render_template('oauth2/logout.html', clients=clients)

commit 3880be9a200d8e5c85ce1bacd1244f9a971a65ab

Fix regression in service overview access behavior
When the service overview was introduced, it was meant to be optional. Thus
if the SERVICES config option was empty (the default), uffd returned 404.

Commit fa67bde (Migrate OAuth2 and API clients to database) introduced the
regression that accessing the service overview page when no services are
visible based on the permissions of the current user (or guest if not logged
in), 404 is returned.

This change fixes the regression and further changes the behavior to improve
consistency. Since fa67bde, the page is relevant to admin users regardless of
the SERVICES config option. Therefore uffd asks for login or reports missing
permissions in all cases it originally returned 404.

uffd/service/templates/service/overview.html
@@ -4,7 +4,7 @@
 
 {% set iconstyle = 'style="width: 1.8em;"'|safe %}
 
-{% if not user %}
+{% if not request.user %}
 <div class="alert alert-warning" role="alert">{{_("Some services may not be publicly listed! Log in to see all services you have access to.")}}</div>
 {% endif %}
 

uffd/service/views.py
@@ -1,3 +1,5 @@
+import functools
+
 from flask import Blueprint, render_template, request, url_for, redirect, current_app, abort
 from flask_babel import lazy_gettext
 
@@ -12,32 +14,40 @@ from uffd.database import db
 
 bp = Blueprint('service', __name__, template_folder='templates')
 
-def service_admin_acl():
+def admin_acl():
 	return request.user and request.user.is_in_group(current_app.config['ACL_ADMIN_GROUP'])
 
-def service_overview_acl():
-	return len(get_services(request.user)) > 0 or service_admin_acl()
+def overview_login_maybe_required(func):
+	@functools.wraps(func)
+	def decorator(*args, **kwargs):
+		if not current_app.config['SERVICES']:
+			return login_required(admin_acl)(func)(*args, **kwargs)
+		if not current_app.config['SERVICES_PUBLIC']:
+			return login_required()(func)(*args, **kwargs)
+		return func(*args, **kwargs)
+	return decorator
+
+def overview_navbar_visible():
+	return get_services(request.user) != [] or admin_acl()
 
 @bp.route('/services/')
-@register_navbar(lazy_gettext('Services'), icon='sitemap', blueprint=bp, visible=service_overview_acl)
+@register_navbar(lazy_gettext('Services'), icon='sitemap', blueprint=bp, visible=overview_navbar_visible)
+@overview_login_maybe_required
 def overview():
 	services = get_services(request.user)
-	if not service_overview_acl():
-		abort(404)
-	banner = current_app.config.get('SERVICES_BANNER')
-	# Set the banner to None if it is not public and no user is logged in
-	if not (current_app.config['SERVICES_BANNER_PUBLIC'] or request.user):
-		banner = None
-	return render_template('service/overview.html', user=request.user, services=services, banner=banner)
+	banner = ''
+	if request.user or current_app.config['SERVICES_BANNER_PUBLIC']:
+		banner = current_app.config['SERVICES_BANNER']
+	return render_template('service/overview.html', services=services, banner=banner)
 
 @bp.route('/service/admin')
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def index():
 	return render_template('service/index.html', services=Service.query.all())
 
 @bp.route('/service/new')
 @bp.route('/service/<int:id>')
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def show(id=None):
 	service = Service() if id is None else Service.query.get_or_404(id)
 	all_groups = Group.query.all()
@@ -46,7 +56,7 @@ def show(id=None):
 @bp.route('/service/new', methods=['POST'])
 @bp.route('/service/<int:id>', methods=['POST'])
 @csrf_protect(blueprint=bp)
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def edit_submit(id=None):
 	if id is None:
 		service = Service()
@@ -68,7 +78,7 @@ def edit_submit(id=None):
 
 @bp.route('/service/<int:id>/delete')
 @csrf_protect(blueprint=bp)
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def delete(id):
 	service = Service.query.get_or_404(id)
 	db.session.delete(service)
@@ -77,7 +87,7 @@ def delete(id):
 
 @bp.route('/service/<int:service_id>/oauth2/new')
 @bp.route('/service/<int:service_id>/oauth2/<int:db_id>')
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def oauth2_show(service_id, db_id=None):
 	service = Service.query.get_or_404(service_id)
 	client = OAuth2Client() if db_id is None else OAuth2Client.query.filter_by(service_id=service_id, db_id=db_id).first_or_404()
@@ -86,7 +96,7 @@ def oauth2_show(service_id, db_id=None):
 @bp.route('/service/<int:service_id>/oauth2/new', methods=['POST'])
 @bp.route('/service/<int:service_id>/oauth2/<int:db_id>', methods=['POST'])
 @csrf_protect(blueprint=bp)
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def oauth2_submit(service_id, db_id=None):
 	service = Service.query.get_or_404(service_id)
 	if db_id is None:
@@ -112,7 +122,7 @@ def oauth2_submit(service_id, db_id=None):
 
 @bp.route('/service/<int:service_id>/oauth2/<int:db_id>/delete')
 @csrf_protect(blueprint=bp)
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def oauth2_delete(service_id, db_id=None):
 	service = Service.query.get_or_404(service_id)
 	client = OAuth2Client.query.filter_by(service_id=service_id, db_id=db_id).first_or_404()
@@ -122,7 +132,7 @@ def oauth2_delete(service_id, db_id=None):
 
 @bp.route('/service/<int:service_id>/api/new')
 @bp.route('/service/<int:service_id>/api/<int:id>')
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def api_show(service_id, id=None):
 	service = Service.query.get_or_404(service_id)
 	client = APIClient() if id is None else APIClient.query.filter_by(service_id=service_id, id=id).first_or_404()
@@ -131,7 +141,7 @@ def api_show(service_id, id=None):
 @bp.route('/service/<int:service_id>/api/new', methods=['POST'])
 @bp.route('/service/<int:service_id>/api/<int:id>', methods=['POST'])
 @csrf_protect(blueprint=bp)
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def api_submit(service_id, id=None):
 	service = Service.query.get_or_404(service_id)
 	if id is None:
@@ -152,7 +162,7 @@ def api_submit(service_id, id=None):
 
 @bp.route('/service/<int:service_id>/api/<int:id>/delete')
 @csrf_protect(blueprint=bp)
-@login_required(service_admin_acl)
+@login_required(admin_acl)
 def api_delete(service_id, id=None):
 	service = Service.query.get_or_404(service_id)
 	client = APIClient.query.filter_by(service_id=service_id, id=id).first_or_404()

tests/test_services.py
@@ -42,12 +42,63 @@ class TestServices(UffdTestCase):
 
 	def test_overview(self):
 		r = self.client.get(path=url_for('service.overview'))
-		dump('service_overview_public', r)
+		dump('service_overview_guest', r)
 		self.assertEqual(r.status_code, 200)
 		self.assertNotIn(b'https://example.com/', r.data)
 		self.login_as('user')
 		r = self.client.get(path=url_for('service.overview'))
-		dump('service_overview', r)
+		dump('service_overview_user', r)
 		self.assertEqual(r.status_code, 200)
 		self.assertIn(b'https://example.com/', r.data)
 
+	def test_overview_disabled(self):
+		self.app.config['SERVICES'] = []
+		# Should return login page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_disabled_guest', r)
+		self.assertEqual(r.status_code, 200)
+		self.assertIn(b'name="password"', r.data)
+		self.login_as('user')
+		# Should return access denied page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_disabled_user', r)
+		self.assertEqual(r.status_code, 403)
+		self.login_as('admin')
+		# Should return (empty) overview page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_disabled_admin', r)
+		self.assertEqual(r.status_code, 200)
+
+	def test_overview_nonpublic(self):
+		self.app.config['SERVICES_PUBLIC'] = False
+		# Should return login page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_nonpublic_guest', r)
+		self.assertEqual(r.status_code, 200)
+		self.assertIn(b'name="password"', r.data)
+		self.login_as('user')
+		# Should return overview page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_nonpublic_user', r)
+		self.assertEqual(r.status_code, 200)
+		self.login_as('admin')
+		# Should return overview page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_nonpublic_admin', r)
+		self.assertEqual(r.status_code, 200)
+
+	def test_overview_public(self):
+		# Should return overview page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_public_guest', r)
+		self.assertEqual(r.status_code, 200)
+		self.login_as('user')
+		# Should return overview page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_public_user', r)
+		self.assertEqual(r.status_code, 200)
+		self.login_as('admin')
+		# Should return overview page
+		r = self.client.get(path=url_for('service.overview'), follow_redirects=True)
+		dump('service_overview_public_admin', r)
+		self.assertEqual(r.status_code, 200)

tests/utils.py
@@ -44,6 +44,9 @@ class UffdTestCase(unittest.TestCase):
 		return Mail.query.filter_by(uid='test').one_or_none()
 
 	def login_as(self, user, ref=None):
+		# It is currently not possible to login while already logged in as another
+		# user, so make sure that we are not logged in first
+		self.client.get(path=url_for('session.logout'), follow_redirects=True)
 		loginname = None
 		password = None
 		if user == 'user':